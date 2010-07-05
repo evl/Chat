3ac0a257101c622244923990c9dc46671e80fd16
Evl <wow.evl@gmail.com>
2010-07-05 21:05:20 +0200

    Added fancy bottom button hide/show!

--- modules/buttons.lua
+++ modules/buttons.lua
@@ -1,24 +1,44 @@
 local addonName, addon = ...
 local config = addon.config
 
-local hide = function(self)
-	self:Hide()
-end
-
 local hideButton = function(button)
-	button:SetScript("OnShow", hide)
+	button:SetScript("OnShow", button.Hide)
 	button:Hide()
 end
 
-if config.hideNavigationButtons then
-	local buttons = {"UpButton", "DownButton", "BottomButton"}
+local updateBottomButton = function(frame)
+	local button = frame.buttonFrame.bottomButton
 
-	for i = 1, NUM_CHAT_WINDOWS do
-		for _, name in pairs(buttons) do
-			local button = _G["ChatFrame" .. i .. "ButtonFrame" .. name]
-			hideButton(button)
-		end
+	if frame:AtBottom() then
+		button:Hide()
+	else
+		button:Show()
 	end
+end
+
+local bottomButtonClick = function(button)
+	updateBottomButton(button:GetParent():GetParent())
+end
+
+if config.hideNavigationButtons then
+	for i = 1, NUM_CHAT_WINDOWS do
+		local frame = _G["ChatFrame" .. i]
+		frame:HookScript("OnShow", updateBottomButton)
+		
+		local buttons = frame.buttonFrame
+		
+		local bottom = buttons.bottomButton
+		bottom:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT")
+		bottom:HookScript("OnClick", bottomButtonClick)
+		bottom:SetAlpha(0.6)
+
+		hideButton(buttons.upButton)
+		hideButton(buttons.downButton)
+		
+		updateBottomButton(frame)
+	end
+	
+	hooksecurefunc("FloatingChatFrame_OnMouseScroll", updateBottomButton)
 end
 
 if config.hideMenuButton then
